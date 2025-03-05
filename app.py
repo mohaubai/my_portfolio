a005b6e530ed446eb63ea65c325a20bf1bdd59e0
mohaubai <mubaid675@gmail.com>
2025-03-05 15:29:39 -0500

    Made bio page dynamic using Jinja

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ def home():
 
 @app.route('/bio')
 def bio():
-    return render_template('bio.html')
+    user_info = {
+        "name": "John Doe",
+        "skills" : "Python, Flask, SQL, Git",
+        "intro": "I am an aspiring software engineer, currently building my project."
+    }
+    return render_template('bio.html', **user_info)
 
 
 if __name__ == '__main__':
